78dea5d30b09b2f236949a6f666baa58aec5b2c8
nikkonov <nikkonov@mail.ru>
2021-06-17 19:35:50 +0300

    HW5

--- log/server_log_config.py
+++ log/server_log_config.py
@@ -7,7 +7,7 @@ logger = logging.getLogger('app.server')
 out_file_formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(module)s - %(message)s ")
 out_stream_formater = logging.Formatter("%(asctime)s - %(levelname)s - %(module)s - %(message)s ")
 
-my_file_handler = TimedRotatingFileHandler("app.server.log", when='m', interval=1, backupCount=0, encoding='utf-8')
+my_file_handler = TimedRotatingFileHandler("app.server.log", when='d', interval=1, backupCount=0, encoding='utf-8')
 my_file_handler.setLevel(logging.DEBUG)
 my_file_handler.setFormatter(out_file_formatter)
 
